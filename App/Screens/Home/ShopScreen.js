98a8f9f506bfe830439c0e16312bee1a69caafc2
Abrar Fahim <abrar9597@gmail.com>
2020-05-01 20:39:15 +0600

    removed search bar and added seacrh button in shopscreen

--- App/Screens/Home/ShopScreen.js
+++ App/Screens/Home/ShopScreen.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import { TextInput, Button, StyleSheet, Text, View, Image, Platform } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -10,27 +10,72 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import {HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/HeaderButton';
 
-import { SearchBar } from 'react-native-elements';
+import { SearchBar, Overlay } from 'react-native-elements';
 import { Drawer } from 'react-native-paper';
 
 import Header from '../../components/Header.js'
 
 
-function ShopScreen() {
-    const [searchText, updateSearchText] = useState('')
+
+
+function ShopScreen({navigation}) {
+    
+
+    const [visible, setVisible] = useState(false);
+
+    function toggleOverlay() {
+        setVisible(!visible);
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                    //<Button onPress={ () => props.navigation.navigate('Cart')} title="My Cart" color="#000"/>
+                    <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                        <Item 
+                            title='CartButton'
+                            iconName='md-cart'
+                            onPress={ () => navigation.navigate('Cart') }
+                        />
+                        <Item 
+                            title='SearchButton'
+                            iconName='md-search'
+                            //onPress={() => navigation.navigate('Shop',{screen:'SearchScreen'})}
+                            onPress={toggleOverlay}
+                        />
+                    </HeaderButtons>
+            
+                    ),
+        })
+    })
+    
     return (
         <View>
-            <SearchBar 
-                placeholder = "Search..."
-                onChangeText = {updateSearchText}
-                value={searchText}
-                platform={Platform.OS}
-                containerStyle={{width: 350, alignItems:'center', justifyContent: 'center'}}
-            />
+
+        <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+            <View>
+                <Text>Search overlay pops up with filters and stuff</Text>
+            </View>
+        </Overlay>
+
+
+
+                    
             <Text> Shop Screen</Text>
         </View>
     );
 
+}
+function SearchScreen(props) {
+    
+    return (
+        <Overlay isVisible={visible}>
+            <Text>Woooah youre so pro omg! </Text>       
+        </Overlay>
+            
+            
+    
+    )
 }
 
 function CategoriesScreen(props) {
@@ -44,24 +89,17 @@ function CategoriesScreen(props) {
 
 function ShopStack(props) {
     const ShopStack = createStackNavigator();
+    
     return (
     
         <ShopStack.Navigator>
             <ShopStack.Screen 
                 name="ShopScreen" 
                 component={ShopScreen} 
-                options = {{
-
-                    headerRight: () => (
-                    //<Button onPress={ () => props.navigation.navigate('Cart')} title="My Cart" color="#000"/>
-                    <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                        <Item 
-                            title='CartButton'
-                            iconName='md-cart'
-                            onPress={ () => props.navigation.navigate('Cart') }
-                        />
-                    </HeaderButtons>
-                    ),
+                options = {
+                    ({navigation, route}) => ({
+                
+                    
                     headerLeft: () => (
                         <HeaderButtons HeaderButtonComponent={HeaderButton}>
                             <Item 
@@ -72,9 +110,12 @@ function ShopStack(props) {
                         </HeaderButtons>
                     )
             
-                }}
+                })
+            }
 
           />
+
+          <ShopStack.Screen name="SearchScreen" component={SearchScreen}/>
         </ShopStack.Navigator>
 
     )
@@ -154,6 +195,8 @@ function DealsScreen() {
 
 export default function ShopStackScreen() {
     const ShopDrawer = createDrawerNavigator();
+
+    
     
     return (
         <ShopDrawer.Navigator 
