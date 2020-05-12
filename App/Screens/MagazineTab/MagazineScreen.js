518e4697b3632f0694181632fbce33d91ec7a6a0
Samin Sharaf Somik <saminsharafsomik@gmail.com>
2020-05-12 08:26:10 +0600

    Magazine Screen Fix

--- App/Screens/MagazineTab/MagazineScreen.js
+++ App/Screens/MagazineTab/MagazineScreen.js
@@ -33,7 +33,7 @@ export function MagazineScreen(props) {
                 <View style={{paddingLeft: 5}}>
                     <TouchableOpacity onPress={() => props.navigation.navigate('OthersProfile')}>
                         <View style={{flexDirection: 'row' , alignItems : 'center'}}>
-                            <View style={{borderRadius: 25, overflow: 'hidden', borderWidth: 2, borderColor: 'black'}}>
+                            <View style={{borderRadius: 25, overflow: 'hidden'}}>
                               <Image style={{width: 40, height: 40 }} source={require('../../assets/Images/tahsan.png')}/>
                             </View>
                             <Text style={{fontWeight: 'bold', fontSize: 20}}> Tahsan </Text>
@@ -42,9 +42,9 @@ export function MagazineScreen(props) {
                 </View>
 
                 <TouchableOpacity style={{paddingTop: 10}}>
-                    <View style={{flexDirection: 'row', width: '100%', height: 300, borderRadius: 30, borderColor: 'black', overflow:'hidden' , borderWidth: 2}}>
-                        <Image  source={require('../../assets/Images/suit.png')} style={{height: '100%', width: '100%', flex:3}}/> 
-                        <Text  style={{padding: 5, flex:1, borderLeftColor: 'black', borderLeftWidth:2, fontWeight: 'bold', backgroundColor: 'grey'}} >   {itemData.item.caption}</Text>    
+                    <View style={{flexDirection: 'column', width: '100%', height: 500, borderRadius: 30, overflow:'hidden'}}>
+                        <Image  source={require('../../assets/Images/suit.png')} style={{height: '80%', width: '100%', flex:7}}/> 
+                        <Text  style={{paddingTop: 10, flex:1, borderLeftColor: 'black',  fontWeight: 'bold', backgroundColor: 'grey'}} >   {itemData.item.caption}</Text>    
                     </View>    
                 </TouchableOpacity>
    
@@ -55,7 +55,7 @@ export function MagazineScreen(props) {
                     <MaterialCommunityIcons name="thumb-up" size={40} color='black'/>
                     </TouchableOpacity>
     
-                    <TextInput placeholder="Comment" style={{flex: 3 ,paddingLeft: 5,height: 40, borderColor: 'black', borderWidth: 2, backgroundColor: 'grey'}}/>
+                    <TextInput placeholder="Comment" style={{flex: 3 ,paddingLeft: 5,height: 40, borderColor: 'black', backgroundColor: 'grey'}}/>
                 </View>
                 
                
@@ -112,7 +112,7 @@ const styles = {
         flex: 1,
         padding : 10,
         margin: 0,
-        height: 500,
+        height: 700,
         width: '100%',
         flexDirection: 'column'
     },
